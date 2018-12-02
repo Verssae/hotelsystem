becb75f90c7c7a2219deae1714a81c83e5381aa1
leejaehee <ipod031@naver.com>
2018-12-02 15:29:56 +0900

    error fixed

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ var app = express()
 var mysql = require('mysql')
 
 /**
- * This middleware provides a consistent API 
+ * This middleware provides a consistent API
  * for MySQL connections during request/response life cycle
- */ 
+ */
 var myConnection  = require('express-myconnection')
 /**
  * Store database credentials in a separate config.js file
  * Load the file/module and its values
- */ 
-var config = require('./config')
+ */
+var config = require('./project/config')
 var dbOptions = {
 	host:	  config.database.host,
 	user: 	  config.database.user,
 	password: config.database.password,
-	port: 	  config.database.port, 
+	port: 	  config.database.port,
 	database: config.database.db,
 	dateStrings: 'date',
 	timezone: 'utc'
@@ -27,18 +27,18 @@ var dbOptions = {
  * single: Creates single database connection which is never closed.
  * pool: Creates pool of connections. Connection is auto release when response ends.
  * request: Creates new connection per new request. Connection is auto close when response ends.
- */ 
+ */
 app.use(myConnection(mysql, dbOptions, 'pool'))
 
 /**
  * setting up the templating view engine
- */ 
+ */
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views');
 /**
  * import routes/index.js
  * import routes/users.js
- */ 
+ */
 var index = require('./routes/index')
 var customers = require('./routes/customers')
 var rooms = require('./routes/rooms')
@@ -47,38 +47,38 @@ var staffs = require('./routes/staffs')
 
 /**
  * Express Validator Middleware for Form Validation
- */ 
+ */
 var expressValidator = require('express-validator')
 app.use(expressValidator())
 
 
 /**
  * body-parser module is used to read HTTP POST data
- * it's an express middleware that reads form's input 
+ * it's an express middleware that reads form's input
  * and store it as javascript object
- */ 
+ */
 var bodyParser = require('body-parser')
 /**
- * bodyParser.urlencoded() parses the text as URL encoded data 
- * (which is how browsers tend to send form data from regular forms set to POST) 
+ * bodyParser.urlencoded() parses the text as URL encoded data
+ * (which is how browsers tend to send form data from regular forms set to POST)
  * and exposes the resulting object (containing the keys and values) on req.body.
- */ 
+ */
 app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
 
 /**
- * This module let us use HTTP verbs such as PUT or DELETE 
+ * This module let us use HTTP verbs such as PUT or DELETE
  * in places where they are not supported
- */ 
+ */
 var methodOverride = require('method-override')
 
 /**
  * using custom logic to override method
- * 
+ *
  * there are other ways of overriding as well
  * like using header & using query value
- */ 
+ */
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
@@ -91,17 +91,17 @@ app.use(methodOverride(function (req, res) {
 /**
  * This module shows flash messages
  * generally used to show success or error messages
- * 
+ *
  * Flash messages are stored in session
- * So, we also have to install and use 
+ * So, we also have to install and use
  * cookie-parser & session modules
- */ 
+ */
 var flash = require('express-flash')
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 app.use(cookieParser('keyboard cat'))
-app.use(session({ 
+app.use(session({
 	secret: 'keyboard cat',
 	resave: false,
 	saveUninitialized: true,
@@ -121,4 +121,3 @@ app.use(express.static('public'));
 app.listen(3000, function(){
 	console.log('Server running at port 3000: http://127.0.0.1:3000')
 })
-
